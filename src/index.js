a5e84eca90e256836549e75c1b0929e5b31365ee
정효진 <jeonghyojin@keunhyojing.local>
2024-01-12 23:59:30 +0900

    connecting store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    root
+);
